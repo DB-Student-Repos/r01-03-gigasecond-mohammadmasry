d742321c5f7731cdeef1e7601b3d19b1a2645c7e
Mohammad El Masri <148862917+mohammadmasry@users.noreply.github.com>
2024-03-20 18:28:31 +0100

    Update lib.rs
    edited code for gigasecond

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
-// Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    let gigasecond_duration = Duration::seconds(1_000_000_000);
+    start + gigasecond_duration
 }
